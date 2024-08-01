b3f2a544f56abf9d1dc26f7d8701eb7c851eaf53
tkms.hrtk@gmail.com <43411481+tkmmobile@users.noreply.github.com>
2024-08-01 18:36:52 +0900

    循環参照の修正

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 import os
 
-LOG_DIR_PATH = os.path.expanduser('~/mqtt_to_cloudwatch_logs')
+LOG_DIR_PATH = os.path.expanduser('log')
 
 BROKER_ADDRESS = "VPN_IP_ADDRESS"  # MQTT BrokerのIPに置き換えてください
 PORT = 1883
@@ -10,7 +10,7 @@ ICCID = "89882280xxxxxxxxx"  # 実際のICCIDに置き換えてください
 TOPIC = f"devices/{ICCID}/data"
 TOPIC_RADIO = f"devices/{ICCID}/radio"
 GOOGLE_SERVER = '8.8.8.8'
-LOG_DIR = 'data/log'
-MQTT_DIR = 'data/mqtt_data'
+LOG_DIR = 'data/network_log'
+MQTT_DIR = 'data/mqtt_network_data'
 RADIO_LOG_DIR = 'data/radio_log'
 MQTT_RADIO_DIR = 'data/mqtt_radio_data'
